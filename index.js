8fad3313f16bc40c028f3f62739785b54cde2aff
Luigi <128727133+luigibuono@users.noreply.github.com>
2023-07-04 19:07:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
-const userRoute = require ("./ROUTES-/userRoutes")
-const authRoute = require("./middleware/auth");
-const productRoute = require("./ROUTES-/productRoutes");
-const cartRoute = require("./routes/cart");
-const orderRoute = require("./ROUTES-/orderRoutes");
+const userRoute = require ("./routes/user")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const orderRoute = require("./routes/order");
+
+//middleware
+app.use(express.json());
+
+const connectDb = require("./db/connect");
 
 const cors = require("cors");
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connection Successfull!"))
-  .catch((err) => {
-    console.log(err);
-  });
+
 
 app.use(cors());
 app.use(express.json());
@@ -27,10 +26,18 @@ app.use("/api/auth", authRoute);
 
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
+
 app.use("/api/orders", orderRoute);
 
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  console.log(`App is listening on port ${port}`);
+  try {
+    await connectDb();
+    console.log("connect to MongoDB");
+  } catch (error) {
+    console.log("Couldn't connect to MongoDB", error);
+  }
 });
