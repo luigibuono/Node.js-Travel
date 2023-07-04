04341e528ded4f82ba8a5ec524d4798b75bb974a
Luigi <128727133+luigibuono@users.noreply.github.com>
2023-07-04 18:39:23 +0200

    Update userController.js

--- CONTROLLERS/userController.js
+++ CONTROLLERS/userController.js
@@ -3,7 +3,7 @@ const {
   verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
-} = require("./verifyToken");
+} = require("../middleware/verifyToken");
 
 const router = require("express").Router();
 
